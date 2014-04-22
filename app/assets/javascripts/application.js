58903d4759d60256e54a05d9609c74eb918e7657
reiz <robert.reiz.81@gmail.com>
2014-04-22 08:33:19 +0200

    Add swagger ui

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,13 +4,6 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
-//= require swagger/lib/jquery-1.8.0.min
-//= require swagger/lib/jquery.slideto.min
-//= require swagger/lib/jquery.wiggle.min
-//= require swagger/lib/jquery.ba-bbq.min
-//= require swagger/lib/handlebars-1.0.rc.1
-//= require swagger/lib/underscore-min.js
-//= require swagger/lib/backbone-min.js
-//= require swagger/lib/swagger.js
-//= require swagger/lib/highlight.7.3.pack.js
-//= require swagger/swagger-ui.js
+//= require jquery
+//= require jquery_ujs
+//
