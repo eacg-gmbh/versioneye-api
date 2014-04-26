f51fde32bce44b51d507571bc4ec088698b12e5f
reiz <robert.reiz.81@gmail.com>
2014-04-26 19:46:00 +0200

    Fixed #2 Recognize Web Session

--- app/controllers/swaggers_controller.rb
+++ app/controllers/swaggers_controller.rb
@@ -2,6 +2,13 @@ class SwaggersController < ApplicationController
 
   def index
     user_api = nil
+    user = current_user
+    if user
+      user_api = Api.where(user_id: user.id).shift
+      if user_api.nil?
+        user_api = Api.create_new( user )
+      end
+    end
 
     @api_key = 'Log in to get your own api token'
     if user_api
@@ -10,26 +17,6 @@ class SwaggersController < ApplicationController
 
     version = params.has_key?(:version) ? params[:version] : 'v2'
     @api_url = "/api/#{version}"
-    # render :layout => 'plain'
   end
-
-  # def index
-  #   user_api = nil
-  #   user = current_user
-  #   if user
-  #     user_api = Api.where(user_id: user.id).shift
-  #     if user_api.nil?
-  #       user_api = Api.create_new( user )
-  #     end
-  #   end
-  #
-  #   @api_key = 'Log in to get your own api token'
-  #   if user_api
-  #     @api_key = user_api.api_key
-  #   end
-  #
-  #   version = params.has_key?(:version) ? params[:version] : 'v2'
-  #   @api_url = "/api/#{version}"
-  # end
 
 end
---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,101 @@
+module SessionsHelper
+
+  def sign_in(user)
+    reset_session
+    cookies.permanent.signed[:remember_token] = [user.id, user.salt]
+    self.current_user = user
+  end
+
+  def sign_out
+    cookies.delete(:remember_token)
+    self.current_user = nil
+  end
+
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def current_user
+    @current_user ||= user_from_remember_token
+  end
+
+  def current_user?(user)
+    return false if user.nil?
+    return nil   if current_user.nil?
+    user.username.eql? current_user.username
+  end
+
+  def signed_in?
+    !current_user.nil?
+  rescue => e
+    Rails.logger.error e.message
+    Rails.logger.error e.backtrace.join("\n")
+    false
+  end
+
+  def signed_in_admin?
+    signed_in? && current_user.admin?
+  rescue => e
+    Rails.logger.error e.message
+    Rails.logger.error e.backtrace.join("\n")
+    false
+  end
+
+  def authenticate
+    return true if signed_in?
+    deny_access
+    false
+  end
+
+  def authenticate_admin
+    return true if signed_in_admin?
+    deny_access
+    false
+  end
+
+  def deny_access
+    store_location
+    redirect_to signin_path, :notice => 'Please sign in to access this page.'
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default, :status => 302)
+    clear_return_to
+  end
+
+  def set_locale
+    locale = 'en'
+    if locale && !locale.empty?
+      I18n.locale = locale
+    elsif I18n.locale.nil?
+      I18n.locale = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    end
+  rescue => e
+    Rails.logger.error e.message
+    Rails.logger.error e.backtrace.join('\n')
+    nil
+  end
+
+  private
+
+    def user_from_remember_token
+      User.authenticate_with_salt(*remember_token)
+    rescue => e
+      Rails.logger.info e.message
+      Rails.logger.error e.backtrace.join("\n")
+      nil
+    end
+
+    def remember_token
+      cookies.signed[:remember_token] || [nil, nil]
+    end
+
+    def store_location
+      session[:return_to] = request.fullpath
+    end
+
+    def clear_return_to
+      session[:return_to] = nil
+    end
+
+end
