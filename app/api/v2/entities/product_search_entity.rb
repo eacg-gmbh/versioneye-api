520c43c071f37205eff6a2ff492ec76b4c860221
reiz <robert.reiz.81@gmail.com>
2014-05-06 00:41:20 +0200

    Cleanup for search entity

--- app/api/v2/entities/product_search_entity.rb
+++ app/api/v2/entities/product_search_entity.rb
@@ -4,15 +4,17 @@ require_relative 'product_entity.rb'
 require_relative 'paging_entity.rb'
 
 module EntitiesV2
+
   class ProductSearchQueryEntity < Grape::Entity
     expose :query    , as: "q"
     expose :languages, as: "lang"
     expose :group_id , as: "g"
   end
+
   class ProductSearchEntity < Grape::Entity
     expose :query  , using: ProductSearchQueryEntity
     expose :entries, using: ProductEntity, as: "results"
     expose :paging , using: PagingEntity
   end
-end
 
+end
