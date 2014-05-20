8ace7f8a4fcbae4417fad5cd56830e3469df13aa
reiz <robert.reiz.81@gmail.com>
2014-05-20 13:20:05 +0200

    Add more attributes to the project_api2

--- app/api/v1/entities/project_entity.rb
+++ app/api/v1/entities/project_entity.rb
@@ -7,13 +7,15 @@ module EntitiesV1
     expose :project_key
     expose :name
     expose :project_type
+    expose :public
     expose :private_project, :as => :private
+    expose :private_project, :as => :private_scm
     expose :period
     expose :source
     expose :dep_number
     expose :out_number
     expose :created_at
     expose :updated_at
-    expose :dependencies, :using => ProjectDependencyEntity, :if => { :type => :full} 
+    expose :dependencies, :using => ProjectDependencyEntity, :if => { :type => :full}
   end
 end
