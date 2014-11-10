bac53f3e83bdb4cac170c06211329533d49ea5b8
reiz <robert.reiz.81@gmail.com>
2014-11-10 19:27:15 +0100

    Remove unused code

--- spec/api/v2/helpers/project_helpers_spec.rb
+++ spec/api/v2/helpers/project_helpers_spec.rb
@@ -6,21 +6,24 @@ describe ProjectHelpers do
   let(:product1) {create(:product, name: "spec_product1", prod_key: "spec_product1", language: 'Ruby', prod_type: 'RubyGems')}
   let(:product2) {create(:product, name: "spec_product2", prod_key: "spec_product2", language: 'Ruby', prod_type: 'RubyGems')}
   let(:project) {create(:project_with_deps, deps_count: 2)}
-  let(:licence2) {create(:license, name: "MIT", prod_key: "spec_product2")}
+  let(:license2) {create(:license, name: "MIT", prod_key: "spec_product2", language: "Ruby")}
 
   describe "add_dependency_licences" do
     before :each do
+      License.delete_all
+
       FactoryGirl.reload
       helper.extend(ProjectHelpers)
       product1.save
       product2.save
-      licence2.version = product2.version
-      licence2.save
+      license2.version = product2.version
+      license2.save
 
       dep_1 = Projectdependency.new(:language => product1.language, :prod_key => product1.prod_key)
       dep_1.save
       dep_2 = Projectdependency.new(:language => product2.language, :prod_key => product2.prod_key)
       dep_2.save
+
       project.dependencies.delete_all
       project.dependencies.push dep_1
       project.dependencies.push dep_2
@@ -31,6 +34,8 @@ describe ProjectHelpers do
     end
 
     it "returns correct licence, when product has one" do
+      License.count.should eq(1)
+
       project.dependencies.first.save
       project.dependencies.last.save
       project.dependencies.count.should eq(2)
@@ -38,6 +43,8 @@ describe ProjectHelpers do
 
       project_after.should_not be_nil
       project_after.dependencies.size.should eq(2)
+
+      License.count.should eq(1)
 
       dep1 = project_after.dependencies[0]
 
