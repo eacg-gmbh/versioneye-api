714d921b5171e105b36d2ff45a8af08b873f20b8
reiz <robert.reiz.81@gmail.com>
2015-02-06 19:30:21 +0100

    Update assets.

---
+++ script/compile_assets.sh
@@ -0,0 +1,11 @@
+export RAILS_ENV=production
+
+mv config/mongoid.yml config/mongoid_save.yml
+mv config/mongoid_assets.yml config/mongoid.yml
+
+rake assets:precompile
+
+mv config/mongoid.yml config/mongoid_assets.yml
+mv config/mongoid_save.yml config/mongoid.yml
+
+export RAILS_ENV=development
