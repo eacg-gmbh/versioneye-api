2f8f36776d6f0b9a3a833f647ca8d0d5dfcbf504
reiz <robert.reiz.81@gmail.com>
2015-04-28 12:06:22 +0200

    Simplify delete Endpoint.

--- app/api/v2/projects_api_v2.rb
+++ app/api/v2/projects_api_v2.rb
@@ -160,11 +160,11 @@ module V2
         error!("Project key can't be empty", 400) if proj_key.nil? or proj_key.empty?
 
         project = Project.by_user(@current_user).where(project_key: proj_key).shift
-        if project.nil? or not destroy_project(project.id)
-          project = Project.by_user(@current_user).where(_id: proj_key).shift
-          if project.nil?
-            error! "Deletion failed because you don't have such project: #{proj_key}", 500
-          end
+        project = Project.by_user(@current_user).where(_id: proj_key).shift if project.nil? 
+        if project.nil? 
+          error! "Deletion failed because you don't have such project: #{proj_key}", 500
+        else 
+          destroy_project(project.id)
         end
 
         {success: true, message: "Project deleted successfully."}
