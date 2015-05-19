021de9225a95a3994b5e4ea358be0bda56c7e889
reiz <robert.reiz.81@gmail.com>
2015-05-19 16:55:18 +0200

    Update config for DatabaseCleaner.

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -3,8 +3,9 @@ require 'database_cleaner'
 RSpec.configure do |config|
 
   config.before(:suite) do
-    DatabaseCleaner.strategy = :truncation
     DatabaseCleaner.orm = "mongoid"
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner[:mongoid].strategy = :truncation
   end
 
   config.before(:each) do
