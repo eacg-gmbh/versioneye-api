041454ace095dc9a63d4cf14e1e97b72d0c63b61
reiz <robert.reiz.81@gmail.com>
2015-05-19 16:54:20 +0200

    Remove Fakeweb

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -4,6 +4,6 @@ require 'webmock'
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/fixtures/vcr_cassettes/'
   c.ignore_localhost = true
-  c.hook_into :webmock # or :fakeweb
+  c.hook_into :webmock 
   c.allow_http_connections_when_no_cassette = true
 end
