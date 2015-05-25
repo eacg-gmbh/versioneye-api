96a49d27a2d1b0dcd6f8e613c102e14df90bd901
reiz <robert.reiz.81@gmail.com>
2015-05-25 16:10:42 +0200

    Remove unused code.

--- app/api/v2/helpers/project_helpers.rb
+++ app/api/v2/helpers/project_helpers.rb
@@ -1,16 +1,6 @@
-
 module ProjectHelpers
 
-  def file_attached?(file_obj)
-    result = false
-
-    if file_obj.nil? == false and file_obj.is_a?(ActionDispatch::Http::UploadedFile )
-      result = true
-    end
-
-    result
-  end
-
+  
   def fetch_project_by_key_and_user(project_key, current_user)
     project = Project.by_user(current_user).where(project_key: project_key).shift
     if project.nil?
@@ -19,6 +9,7 @@ module ProjectHelpers
     project
   end
 
+  
   def destroy_project(project_id)
     project = Project.find_by_id(project_id)
     project.dependencies.each do |dep|
@@ -27,10 +18,12 @@ module ProjectHelpers
     project.remove
   end
 
+  
   def upload_and_store file
     ProjectImportService.import_from_upload file, current_user, true
   rescue => e 
     return e.message
   end
+
 
 end
