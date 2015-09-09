45a3e63acff635cb5bc47b7385364f72a40664dd
reiz <robert.reiz.81@gmail.com>
2015-09-09 14:47:58 +0200

    Remove unused code.

--- app/api/v2/helpers/paging_helpers.rb
+++ app/api/v2/helpers/paging_helpers.rb
@@ -12,29 +12,9 @@ module PagingHelpers
   def make_paging_for_references( page, total_count )
     total_pages = total_count / 30
     total_pages += total_count % 30
-    SearchResults.new({ 
+    SearchResults.new({
       current_page: page,
       per_page: 30,
-      total_entries: total_count,
-      total_pages: total_pages 
-    })
-  end
-
-  def make_paging_hash(query_results)
-    SearchResults.new({
-      current_page: query_results.current_page,
-      per_page: query_results.per_page,
-      total_entries: query_results.total_entries,
-      total_pages: query_results.total_pages
-    })
-  end
-
-  def list_to_paging(results, page = 1, total_count = 1, per_page = 30)
-    total_pages = total_count / per_page
-    total_pages += 1 if (results.count % per_page)
-    SearchResults.new({
-      current_page: page,
-      per_page: per_page,
       total_entries: total_count,
       total_pages: total_pages
     })
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,16 +1,5 @@
 module SessionsHelper
 
-  def sign_in(user)
-    reset_session
-    cookies.permanent.signed[:remember_token] = [user.id, user.salt]
-    self.current_user = user
-  end
-
-  def sign_out
-    cookies.delete(:remember_token)
-    self.current_user = nil
-  end
-
   def current_user=(user)
     @current_user = user
   end
@@ -33,22 +22,8 @@ module SessionsHelper
     false
   end
 
-  def signed_in_admin?
-    signed_in? && current_user.admin?
-  rescue => e
-    Rails.logger.error e.message
-    Rails.logger.error e.backtrace.join("\n")
-    false
-  end
-
   def authenticate
     return true if signed_in?
-    deny_access
-    false
-  end
-
-  def authenticate_admin
-    return true if signed_in_admin?
     deny_access
     false
   end
@@ -61,19 +36,6 @@ module SessionsHelper
   def redirect_back_or(default)
     redirect_to(session[:return_to] || default, :status => 302)
     clear_return_to
-  end
-
-  def set_locale
-    locale = 'en'
-    if locale && !locale.empty?
-      I18n.locale = locale
-    elsif I18n.locale.nil?
-      I18n.locale = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
-    end
-  rescue => e
-    Rails.logger.error e.message
-    Rails.logger.error e.backtrace.join('\n')
-    nil
   end
 
   private
