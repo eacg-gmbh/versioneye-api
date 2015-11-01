e2b2023e1720c7a3f1b9c55c32b3d06cd0f4bb5d
reiz <robert.reiz.81@gmail.com>
2015-11-01 19:14:59 +0100

    Add optional prod_key to the sv endpoint.

--- app/api/v2/security_api_v2.rb
+++ app/api/v2/security_api_v2.rb
@@ -18,20 +18,24 @@ module V2
       desc 'Security Vulnerabilities'
       params do
         requires :language, :type => String, :desc => %q[Filter by programming languages]
+        optional :prod_key, :type => String, :desc => %q[prod_key of the package]
         optional :page, :type => Integer, :desc => "Specify page for paging", :regexp => /^[\d]+$/
       end
       get '/' do
+        prod_key = decode_prod_key(params[:prod_key])
         lang = get_language_param(params[:language])
         page = 0
         page = (params[:page].to_i - 1) if !params[:page].to_s.empty?
         per_page = 30
         skip = per_page * page
-        data = SecurityVulnerability.where(:language => lang).skip(skip).limit(per_page)
+        data = SecurityVulnerability.where(:language => lang)
+        if !prod_key.to_s.empty?
+          data = data.where(:prod_key => prod_key )
+        end
+        data = data.skip(skip).limit(per_page)
         total_count = data.count
-        # present data, with: EntitiesV2::SecurityVulnerabilityEntity
-
-        paging     = make_paging_for_references( page + 1, total_count )
-        results    = SearchResults.new paging: paging, entries: data
+        paging      = make_paging_for_references( page + 1, total_count )
+        results     = SearchResults.new paging: paging, entries: data
         present results, with: EntitiesV2::SecurityResultEntity
       end
 
