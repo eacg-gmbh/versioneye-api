8adf87455764c41241782f6ba8fec957c85603d2
reiz <robert.reiz.81@gmail.com>
2016-06-03 10:12:52 +0200

    Update Script section.

--- script/compile_assets.sh
+++ script/compile_assets.sh
@@ -8,4 +8,13 @@ rake assets:precompile
 mv config/mongoid.yml config/mongoid_assets.yml
 mv config/mongoid_save.yml config/mongoid.yml
 
+cp app/assets/javascripts/*       public/assets/
+cp app/assets/javascripts/libs/*  public/assets/libs/
+cp app/assets/javascripts/plots/* public/assets/plots/
+cp app/assets/images/*.png public/assets/
+cp app/assets/images/*.jpg public/assets/
+cp app/assets/images/*.gif public/assets/
+cp app/assets/images/language/* public/assets/language/
+cp app/assets/images/logos/* public/assets/logos/
+
 export RAILS_ENV=development
