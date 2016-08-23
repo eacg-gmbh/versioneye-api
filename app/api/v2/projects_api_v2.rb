179ec4b10976d76b52a8049a9b64432977b6c7ba
reiz <robert.reiz.81@gmail.com>
2016-08-23 21:12:18 +0200

    Clean up.

--- app/api/v2/projects_api_v2.rb
+++ app/api/v2/projects_api_v2.rb
@@ -93,12 +93,12 @@ module V2
             ]
       }
       params do
-        requires :upload, type: Hash, desc: "Project file - [maven.pom, Gemfile ...]"
+        requires :upload    , :type => Hash,   :desc => "Project file - [maven.pom, Gemfile ...]"
         optional :visibility, :type => String, :desc => "By default 'public'. If 'public' everybody can see the project."
-        optional :name, :type => String, :desc => "The name of the VersionEye project. By default it is the filename."
-        optional :orga_name, :type => String, :desc => "The name of the organisation this project should be assigned to."
-        optional :team_name, :type => String, :desc => "The name of the team in the organisation this project should be assigned to."
-        optional :temp, :type => String, :desc => "If 'true' this project will not show up in the UI and gets removed later."
+        optional :name      , :type => String, :desc => "The name of the VersionEye project. By default it is the filename."
+        optional :orga_name , :type => String, :desc => "The name of the organisation this project should be assigned to."
+        optional :team_name , :type => String, :desc => "The name of the team in the organisation this project should be assigned to."
+        optional :temp      , :type => String, :desc => "If 'true' this project will not show up in the UI and gets removed later."
       end
       post do
         rate_limit
