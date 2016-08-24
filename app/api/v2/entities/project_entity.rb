bac33aaa43013c6f6a3c2fb749e144dc731305c6
reiz <robert.reiz.81@gmail.com>
2016-08-24 10:18:36 +0200

    Exposing project child and parent ids

--- app/api/v2/entities/project_entity.rb
+++ app/api/v2/entities/project_entity.rb
@@ -25,5 +25,7 @@ module EntitiesV2
     expose :updated_at
     expose :license_whitelist_name, :as => :license_whitelist
     expose :projectdependencies, :as => :dependencies, :using => ProjectDependencyEntity, :if => { :type => :full}
+    expose :child_ids
+    expose :parent_id
   end
 end
