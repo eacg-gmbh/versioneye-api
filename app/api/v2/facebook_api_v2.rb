a75ebe7847f4e8433668bf3e5f33e1531f2fabe6
reiz <robert.reiz.81@gmail.com>
2017-01-03 18:45:15 +0100

    Update logging

--- app/api/v2/facebook_api_v2.rb
+++ app/api/v2/facebook_api_v2.rb
@@ -30,7 +30,7 @@ module V2
       content_type :txt, 'application/json'
       desc "ping pong"
       post '/ping' do
-        Rails.logger.info "request: #{request}"
+        Rails.logger.info "request.body: #{request.body.read}"
         Rails.logger.info "params: #{params}"
         Rails.logger.info "object: #{params['object']}"
         Rails.logger.info "entry: #{params['entry']}"
