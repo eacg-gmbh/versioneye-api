c7b0c38877b67100c645fdbecce16ada16c43170
Stefan Pokorny <stefan.pokorny@eacg.de>
2018-07-12 13:06:47 +0200

    Updated config to add aws and secrets storage

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,14 @@ RUN rm -Rf /app; \
 
 ADD . /app
 
+# Install the AWS CLI
+RUN apt-get update && apt-get -y install python python-dev python-pip python-setuptools groff less curl zip unzip && cd /tmp && \
+    curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" \
+    -o "awscli-bundle.zip" && \
+    unzip awscli-bundle.zip && \
+    ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws && \
+    rm awscli-bundle.zip && rm -rf awscli-bundle
+
 RUN cd /app/ && bundle install;
 
 EXPOSE 9090
--- start.sh
+++ start.sh
@@ -5,6 +5,15 @@
 # if you want to make self signed certificates
 # available
 
-sudo update-ca-certificates;
+#sudo update-ca-certificates;
+
+# Check that the environment variable has been set correctly
+if [ -z "$SECRETS_BUCKET_NAME" ]; then
+  echo >&2 'error: missing SECRETS_BUCKET_NAME environment variable'
+  exit 1
+fi
+
+# Load the S3 secrets file contents into the environment variables
+eval $(aws s3 cp s3://${SECRETS_BUCKET_NAME}/credentials.txt - | sed 's/^/export /')
 
 cd /app; bundle exec puma -C config/puma.rb
